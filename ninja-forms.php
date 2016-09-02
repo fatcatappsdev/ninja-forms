0447960b086737f38e94747cad693132ecfc6e86
Kyle B. Johnson <kyle@wpninjas.com>
2016-09-02 16:22:46 -0400

    default to v3 and import contact form on activation.

--- ninja-forms.php
+++ ninja-forms.php
@@ -23,10 +23,6 @@ function ninja_forms_three_table_exists(){
     return ( $wpdb->get_var("SHOW TABLES LIKE '$table_name'") == $table_name );
 }
 
-if( version_compare( get_option( 'ninja_forms_version', '0.0.0' ), '3', '<' ) || ! ninja_forms_three_table_exists() ) {
-    update_option( 'ninja_forms_load_deprecated', TRUE );
-}
-
 if( get_option( 'ninja_forms_load_deprecated', FALSE ) && ! ( isset( $_POST[ 'nf2to3' ] ) && ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) ){
 
     include 'deprecated/ninja-forms.php';
@@ -565,6 +561,9 @@ if( get_option( 'ninja_forms_load_deprecated', FALSE ) && ! ( isset( $_POST[ 'nf
         public function activation() {
             $migrations = new NF_Database_Migrations();
             $migrations->migrate();
+
+            $form = Ninja_Forms::template( 'formtemplate-contactform.nff', array(), TRUE );
+            Ninja_Forms()->form()->import_form( $form );
         }
 
     } // End Class Ninja_Forms
